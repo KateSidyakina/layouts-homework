d5d98426b97ec29b31fbb9e66fd4e3e8f93ccc28
Kate Sidyakina <therinbae@gmail.com>
2023-12-07 00:31:04 +0300

    layouts-homework fixed

--- src/components/CardsView/CardsView.jsx
+++ src/components/CardsView/CardsView.jsx
@@ -3,7 +3,7 @@ import ShopCard from "../ShopCard/ShopCard";
 export default function CardsView({cards, style}) {
   return (
     <div className='cards' style={style}>
-      {cards.map((card) => <ShopCard card={card}/>)}
+      {cards.map((card, index) => <ShopCard key={index + card.name} card={card}/>)}
     </div>
   )
 }
--- src/components/ListView/ListView.jsx
+++ src/components/ListView/ListView.jsx
@@ -3,7 +3,7 @@ import ShopItem from "../ShopItem/ShopItem";
 export default function ListView({items, style}) {
   return (
     <div className='items' style={style}>
-      {items.map((item) => <ShopItem item={item}/>)}
+      {items.map((item, index) => <ShopItem key={item.name + index} item={item}/>)}
     </div>
   )
 }
